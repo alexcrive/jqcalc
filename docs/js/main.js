cd96bed803a56a54cd4bb29f420e02dd65bbeec6
alex_crive <alexcrive95@yandex.ru>
2019-08-27 12:26:02 +0300

    cars to snickers

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,94 +1,81 @@
 $(document).ready(function(){
 
-	// jQuery - Авто калькулятор
+let	imgPath = $('.img-product img');
+let	colorButton = $('#colorSelector .colorItem');
 
-	var modelSpecs,
-		modelPrice,
-		modelSpecsHolder,
-		modelPriceHolder,
-		modelPriceUSDHolder;
+//color changing
+colorButton.click(function() {
+	let imgChange = $(this).attr('data-img-path');
 
-	modelSpecsHolder = $('#modelSpecs');
-	modelPriceHolder = $('#modelPrice');
-	modelPriceUSDHolder = $('#modelPriceUSD');
-	
-	modelPrice = 0;
-	modelSpecs = '';
-
-	// При старте страницы
-	calculatePrice();
-	compileSpecs();
-
-
-	// После переключения радио кнопок
-	$('#autoForm input').on('change', function(){
-		calculatePrice();
-		compileSpecs();
-		calculateUSD();
+	imgPath.fadeOut(300, function(){
+		imgPath.attr('src', imgChange);
+		imgPath.fadeIn(300);
 	});
+});
+
+let productCountHolder = $('.product-count');
+let productDeliveryHolder = $('.product-delivery');
+let productPriceHolder = $('#productPrice');
+let productPriceHolderUSD = $('#productPriceUSD');
+
+// summ the result in rub
+function priceCalc() {
+	productNumbers = $('input[name=productCount]:checked', '#formCalc').val();
+	productDelivers = $('input[name=delivery]:checked', '#formCalc').val();
+	productNumbers = parseInt(productNumbers);
+	productDelivers = parseInt(productDelivers);
+
+	productPrice = parseInt(productPrice);
+	productPrice = productNumbers +  productDelivers;
+
+	productPriceHolder.text(spaceEveryThree(productPrice) + ' рублей');
+
+};
+// add  text for delivery
+function deliveryText() {
+	productDelivery = $('input[name=delivery]:checked + label', '#formCalc').text();
+	productDeliveryHolder.text(productDelivery);
+};
+// add  text for count
+function countText() {
+	productCount = $('input[name=productCount]:checked + label', '#formCalc').text();
+	productCountHolder.text(productCount);
+};
+
+// USD to RUB
+let USDtoRub;
+
+$.getJSON("https://www.cbr-xml-daily.ru/daily_json.js", function(data) {
+    //$('#USD').text(data.Valute.USD.Value.toFixed(2));
+    USDtoRub = data.Valute.USD.Value.toFixed(0);
+	CalcUSDtoRub();
+});
+// rub / usd
+function CalcUSDtoRub() {
+	let priceUSD = productPrice / USDtoRub;
+	productPriceHolderUSD.text(spaceEveryThree(priceUSD.toFixed(0)) + ' $')
+}
+
+//count with changing
+priceCalc();
+countText();
+deliveryText();
+CalcUSDtoRub();
+$('#formCalc input').change(function(){
+	priceCalc();
+	countText();
+	deliveryText();
+	CalcUSDtoRub();
+});
 
 
 
-	// ВЫБОР ЦВЕТА - на цену не влияет
-	$('#colorsSelector .colorItem').on('click', function(){
-		var imgPath = $(this).attr('data-img-path');
-		$('#imgHolder img').attr('src', imgPath);
-	});
 
 
-	function calculatePrice(){
-		var modelPriceEngine = $('input[name=engine]:checked', '#autoForm').val();
-		var modelPriceTransmission = $('input[name=transmission]:checked', '#autoForm').val();
-		var modelPricePackage = $('input[name=package]:checked', '#autoForm').val();
-		
-		modelPriceEngine = parseInt(modelPriceEngine);
-		modelPriceTransmission = parseInt(modelPriceTransmission);
-		modelPricePackage = parseInt(modelPricePackage);
-		
-		modelPrice = modelPriceEngine + modelPriceTransmission + modelPricePackage;
-		// alert(modelPrice);
-		modelPriceHolder.text( addSpace(modelPrice) + ' рублей');
-	};
-
-	function compileSpecs(){
-		modelSpecs = $('input[name=engine]:checked + label', '#autoForm').text();
-		modelSpecs = modelSpecs + ', ' + $('input[name=transmission]:checked + label', '#autoForm').text();
-		modelSpecs = modelSpecs + ', ' + $('input[name=package]:checked + label', '#autoForm').text() + '.';
-		// alert(modelSpecs);
-		modelSpecsHolder.text( modelSpecs );
-	};
-
-	function addSpace(nStr) {
-	    nStr += '';
-	    x = nStr.split('.');
-	    x1 = x[0];
-	    x2 = x.length > 1 ? '.' + x[1] : '';
-	    var rgx = /(\d+)(\d{3})/;
-	    while (rgx.test(x1)) {
-	        x1 = x1.replace(rgx, '$1' + ' ' + '$2');
-	    }
-	    return x1 + x2;
-	}
-
-	// Получаем курс валют
-	var currencyUrl = 'https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+"USDRUB,EURRUB"&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
-	var rurUsdRate = 0;
-
-	$.ajax({
-		url: currencyUrl,
-		cache: false,
-		success: function(html){
-			console.log( html.query.results.rate[0].Rate );
-			rurUsdRate = html.query.results.rate[0].Rate;
-			calculateUSD();
-		} 
-	});
-
-	function calculateUSD(){
-		var modelPriceUSD = modelPrice / rurUsdRate;
-		// alert(modelPriceUSD);
-		modelPriceUSDHolder.text( '$ ' + addSpace( modelPriceUSD.toFixed(0) ) );
-	}
-
+//func for spacing between every 3 sym
+function spaceEveryThree(num) {
+    var n = num.toString();
+    return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
+}
 
 });
